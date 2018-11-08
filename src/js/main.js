b449a23a33dd25c7adeb59cdb119bd318937c115
Neverov Nikolay <neverov.nikolay@gmail.com>
2018-11-09 00:16:51 +0300

    init

---
+++ src/js/main.js
@@ -0,0 +1,141 @@
+/*jshint esversion: 6 */
+'use strict';
+
+$(document).ready(function () {
+    var dropZone = $('#dropZone');
+
+
+    if (typeof (window.FileReader) === 'undefined') {
+        dropZone.text('Не поддерживается браузером!');
+        dropZone.addClass('error');
+    }
+
+    dropZone[0].ondragover = function () {
+        dropZone.addClass('hover');
+        return false;
+    };
+
+    dropZone[0].ondragleave = function () {
+        dropZone.removeClass('hover');
+        return false;
+    };
+
+    dropZone[0].ondrop = function (event) {
+        event.preventDefault();
+        dropZone.removeClass('hover');
+        dropZone.addClass('drop');
+
+        const file = event.dataTransfer.files[0];
+        const reader = new FileReader();
+        //reader.onload = onFileLoad;
+        reader.onload = function (e) {
+            onFileLoad(e, file.name);
+        };
+        reader.readAsDataURL(file);
+    };
+});
+
+function log(text) {
+    console.log(text);
+}
+
+function dropZoneOnClick() {
+    $('#fileInput').trigger('click');
+}
+
+function fileChange(e) {
+
+    var reader = new FileReader();
+    var files = e.dataTransfer ? e.dataTransfer.files : e.target.files;
+
+    reader.onload = function (e) {
+        onFileLoad(e, files[0].name);
+    };
+    reader.readAsDataURL(files[0]);
+}
+
+/**
+ * Загрузка файла
+ * @param {any} e
+ * @param {any} fileName
+ */
+function onFileLoad(e, fileName) {
+
+    const data = e.target.result;
+    const fileText = atob(data.split(',')[1]);
+
+    const puzzle = new Puzzle(fileText);
+
+    puzzle.Solve();
+    drawSolvedPuzzle(puzzle);
+}
+
+function drawSolvedPuzzle(puzzle){
+
+    const puzzleContainer = document.querySelector('#solvePuzzle');
+    
+    puzzleContainer.innerHTML = '';
+
+    const table = document.createElement('table');
+    const firstRow = createFirstRow(puzzle);
+
+    table.appendChild(firstRow);
+
+    for (let i = 0; i < puzzle.RowCount; i++) {
+        let row = document.createElement('tr');
+
+        const rowInfo = puzzle.RowsInfo[i];
+        let cellInfo = createCellInfo(rowInfo);
+        row.appendChild(cellInfo);
+
+        for (let j = 0; j < puzzle.ColumnCount; j++){
+            let cell = document.createElement('td');
+            cell.style.backgroundColor = puzzle.Cells[i][j].Color;
+            cell.innerHTML = puzzle.Cells[i][j].Info; // TODO
+
+            row.appendChild(cell);
+        }
+        
+        table.appendChild(row);
+    }
+
+    puzzleContainer.appendChild(table);
+}
+
+function createFirstRow(puzzle){
+
+    const row = document.createElement('tr');
+
+    const emptyCell = document.createElement('td');
+    row.appendChild(emptyCell);
+
+    for (let i = 0; i < puzzle.ColumnCount; i++) {
+        
+        const columnInfo = puzzle.ColumnsInfo[i];
+        let cell = createCellInfo(columnInfo);
+        
+        row.appendChild(cell);
+    }
+
+    return row;
+}
+
+function createCellInfo(info){
+    const cell = document.createElement('td');
+    const span = document.createElement('span');
+    
+    for(let j = 0; j < info.Groups.length; j++){
+        
+        let div = document.createElement('div');
+
+        div.innerHTML = info.Groups[j].Count;
+        div.style.backgroundColor = info.Groups[j].Color;
+
+        span.appendChild(div);
+        
+    }
+
+    cell.appendChild(span);
+
+    return cell;
+}
---
+++ src/js/puzzle.js
@@ -0,0 +1,135 @@
+/*jshint esversion: 6 */
+'use strict';
+
+class Puzzle{
+
+    constructor(puzzleText){
+
+        this._parsePuzzle(puzzleText);
+    }
+
+    _parsePuzzle(puzzleText){
+
+        const lines = puzzleText
+            .split('\n')
+            .map(s => s.replace('\r', ''))
+            .filter(l => l !== '');
+
+        /**
+         * Формат файла:
+         * n - количество цветов
+         * n строк с hex цветами
+         * x y размер сетки
+         * x строк - с описанием строк
+         * y строк - с описанием столбцов
+         * 
+         * Столбцы и строки:
+         * n - количество клеток
+         * клетка - l r g b
+         * l - длина, rgb цвет в int формате
+         */
+
+        this._parseColors(lines);
+        this._parseGridSize(lines);
+        this._parseRows(lines);
+        this._parseColumns(lines);
+    }
+
+    _parseColors(lines){
+
+        const colorsCount = parseInt(lines[0]);
+        lines.splice(0, 1); // Удаляем строку с количеством цветов
+
+        this.Colors = lines.splice(0, colorsCount);
+    }
+
+    _parseGridSize(lines){
+
+        const rc = lines[0].split(' ');
+
+        this.RowCount = parseInt(rc[0]);
+        this.ColumnCount = parseInt(rc[1]);
+
+        lines.splice(0, 1); // Удаляем строку с размером таблицы
+    }
+
+    _parseRows(lines){
+
+        this.RowsInfo = lines
+            .splice(0, this.RowCount)
+            .map(this._parseCellsInfo.bind(this));
+    }
+
+    _parseColumns(lines){
+
+        this.ColumnsInfo = lines
+            .splice(0, this.ColumnCount)
+            .map(this._parseCellsInfo.bind(this));
+    }
+
+    _parseCellsInfo(cellsInfo){
+
+        const s = cellsInfo.split(' ');
+
+        const count = parseInt(s[0]);
+        const groups = new Array(count);
+        
+        for (let i = 0; i < count; i++) {
+
+            let left = i * 4 + 1;
+            let groupInfo = s.slice(left, left + 4);
+
+            groups[i] = this._parseGroupInfo(groupInfo);
+        }
+
+        const colorBits = groups
+            .map(g => g.ColorIndex)
+            .filter(onlyUnique)
+            .reduce((prev, curr) => prev | (1 << curr), 0);
+
+        return {
+            Groups: groups,
+            ColorBits: colorBits
+        };
+    }
+
+    _parseGroupInfo(groupInfo){
+
+        let cellCount = parseInt(groupInfo[0]);
+        let r = parseInt(groupInfo[1]);
+        let g = parseInt(groupInfo[2]);
+        let b = parseInt(groupInfo[3]);
+        let hexColor = rgbToHex(r, g, b);
+
+        return {
+            Count: cellCount,
+            Color: hexColor,
+            ColorIndex: this._getColorIndex(hexColor)
+        };
+    }
+
+    _getColorIndex(hexColor){
+
+        return this.Colors.indexOf(hexColor);
+    }
+
+    Solve(){
+
+        this._createEmptyCells();
+    }
+
+    _createEmptyCells(){
+
+        this.Cells = new Array(this.RowCount);
+
+        for (let i = 0; i < this.RowCount; i++) {
+            this.Cells[i] = new Array(this.ColumnCount);
+
+            for (let j = 0; j < this.ColumnCount; j++) {
+                this.Cells[i][j] = {
+                    Info: this.RowsInfo[i].ColorBits & this.ColumnsInfo[j].ColorBits
+                }
+            }
+        }
+    }
+}
